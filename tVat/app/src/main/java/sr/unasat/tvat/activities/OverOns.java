05589a306487cfce0bc34048ccb2100192144033
ShivashMahespalsingh <mahespalsingh.shivash@outlook.com>
2017-02-15 19:34:04 -0300

    Over ons page en foto openingstijden

---
+++ tVat/app/src/main/java/sr/unasat/tvat/activities/OverOns.java
@@ -0,0 +1,15 @@
+package sr.unasat.tvat.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import sr.unasat.tvat.R;
+
+public class OverOns extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_over_ons);
+    }
+}
